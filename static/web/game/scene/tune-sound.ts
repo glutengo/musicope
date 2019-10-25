e85a35aed64c85469035323dd19db2686efdce74
Markus Glutting <markus.glutting@sprylab.com>
2019-10-25 12:26:06 +0200

    Stop tune sounds when note is released
    * Use multiple ToneJS sampler instances instead of one
    * Implement stop and stop_all methods for TuneSound
    * Add minimum play length so sound is always played

--- static/web/game/scene/tune-sound.ts
+++ static/web/game/scene/tune-sound.ts
@@ -1,44 +1,64 @@
-import { ISound } from "./sound";
-import * as Tune from "tune";
-import * as Tone from "tone";
-import { config } from "../../config/config"
+import {Dictionary, ISound} from "./sound"
+import * as Tune from "tune"
+import * as Tone from "tone"
+import {config} from "../../config/config"
 
-const BASE_NOTE = 'C4';
-const BASE_NOTE_PATH = '/c4.mp3';
+const BASE_NOTE = 'C4'
+const BASE_NOTE_PATH = '/c4.mp3'
+const MINIMUM_PLAY_LENGTH = 200;
 
 export class TuneSound implements ISound {
 
-  // TODO: Tune type
-  tune: any;
-  // TODO: Tone type
-  piano: any;
-  context: AudioContext;
+    // TODO: Tune type
+    tune: any
+    // TODO: piano type
+    pianos: Dictionary<any> = {};
+    context: AudioContext
 
-  constructor() {
-        this.tune = new Tune();
-        this.tune.loadScale(config.s_tuneScale);
-        this.tune.mode.output = 'MIDI';
-        this.piano = new Tone.Sampler({
-          [BASE_NOTE]: BASE_NOTE_PATH
-        });
-        this.piano.toMaster();
-        this.context = new AudioContext();
-  }
+    notes: any = {};
 
-  start(id: number): void {
-    if (id) {
-      this.piano.triggerAttack(BASE_NOTE);
-      this.piano.pitch = this.tune.note(id-this.tune.scale.length)-this.tune.key;
+    constructor() {
+        this.tune = new Tune()
+        this.tune.loadScale(config.s_tuneScale)
+        this.tune.mode.output = 'MIDI'
+
+        for (let note = 1; note < 139; note++) {
+            this.pianos[note] = new Tone.Sampler({
+                [BASE_NOTE]: BASE_NOTE_PATH
+            })
+            this.pianos[note].toMaster()
+            this.pianos[note].pitch = this.tune.note(note - this.tune.scale.length) - this.tune.key
+        }
+        this.context = new AudioContext()
     }
-  }
 
-  stop(id: number): void {
-    // TODO: implement stop
-  }
+    start(id: number): void {
+        if (this.pianos[id]) {
+            this.pianos[id].triggerAttack(BASE_NOTE)
+            window.clearTimeout(this.pianos[id].timeout)
+        }
+    }
 
-  stop_all(): void {
-    // TODO: implement stop all
-  }
+    stop(id: number): void {
+        if (this.pianos[id]) {
+            const playLength = (Date.now() - this.notes[id]) > MINIMUM_PLAY_LENGTH ? 0 : MINIMUM_PLAY_LENGTH
+            this.pianos[id].timeout = setTimeout(() => this.pianos[id].triggerRelease(), playLength)
+        }
+    }
+
+    stop_all(): void {
+        for (let note in this.pianos) {
+            this.pianos[note].triggerRelease();
+        }
+    }
+
+    pause() {
+        this.context.suspend();
+    }
+
+    resume() {
+        this.context.suspend();
+    }
 
 
 }
